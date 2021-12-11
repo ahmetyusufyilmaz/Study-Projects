62d0317dd99366806ccb2135f95ea0b27002165d
ahmetyusufyilmaz <94035519+ahmetyusufyilmaz@users.noreply.github.com>
2021-12-11 23:06:52 +0300

    Update

---
+++ C# Console Applications/CarpimTablosu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CarpimTablosu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = 1, s2 = 1;
+
+            while (s1 <= 20)
+            {
+                while (s2 <= 10)
+                {
+                    Console.WriteLine($"{s1}*{s2}={s1 * s2}");
+                    s2++;
+                }
+                s1++;
+                s2 = 1;
+                Console.WriteLine("_________\n");
+
+            }
+
+        }
+    }
+}
