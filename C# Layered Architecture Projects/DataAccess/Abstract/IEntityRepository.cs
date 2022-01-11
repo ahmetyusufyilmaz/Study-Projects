5e9987c5169a5b58cf70e1a62b30bbf7ee1bf267
ahmetyusufyilmaz <94035519+ahmetyusufyilmaz@users.noreply.github.com>
2022-01-11 20:53:05 +0300

    Update

---
+++ C# Layered Architecture Projects/DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    //generic constraint (kısıtlama)
+    //class: referans tip
+    //IEntity: IEntity olabilir veya IEntity implemente eden bir nesne olabilir
+    //new() : new'lenebilir olmalı
+    public interface IEntityRepository<T>where T:class,IEntity,new()
+    {
+        List<T> GetAll(Expression<Func<T,bool>>filter=null);
+
+        T Get();
+
+        void Add(T entity);  // Interface methodları default public'tir.
+        void Update(T entity);
+        void Delete(T entity);
+        Product Get(Expression<Func<Product, bool>> filter);
+    }
+}
